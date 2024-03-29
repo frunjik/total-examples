ae2b50a0d11c40296b3013a3fbe879e9a48aae01
Peter Širka <petersirka@gmail.com>
2024-03-29 19:36:23 +0100

    Added improvements.

--- definitions/init.js
+++ definitions/init.js
@@ -1,2 +1,2 @@
 // UI Components
-COMPONENTATOR('ui', 'exec,errorhandler,locale,notify,importer,page,intranetcss,layout,loading,selected', true);
+COMPONENTATOR('ui', 'exec,errorhandler,locale,notify,importer,page,intranetcss,layout,loading,selected,listmenu,objecttree,paper', true);
